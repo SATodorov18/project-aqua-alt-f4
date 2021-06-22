a6f59f8afe73d105015f1b12ce2f41736c70f41f
Velimir Dimitrov <VVDimitrov19@codingburgas.bg>
2021-06-22 22:25:42 +0300

    Make Navbar Responsive

--- Project/Website/JS/MainPage.js
+++ Project/Website/JS/MainPage.js
@@ -1,2 +1,7 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querysSelector('.nav-menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menu.classList.toggle('active');
+})
