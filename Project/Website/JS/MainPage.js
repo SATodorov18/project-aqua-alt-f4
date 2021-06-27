744e6272b953625e8f6f260dc90bfc30ec75e1cd
Velimir Dimitrov <VVDimitrov19@codingburgas.bg>
2021-06-27 09:39:23 +0300

    Finish Navbar

--- Project/Website/JS/MainPage.js
+++ Project/Website/JS/MainPage.js
@@ -4,3 +4,11 @@ const openMenu = document.querySelector('.openMenu');
 
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
