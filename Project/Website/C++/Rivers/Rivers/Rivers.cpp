a83cea842987e0149d57a5f9fe4d6be6bc1a323f
Velimir Dimitrov <VVDimitrov19@codingburgas.bg>
2021-06-27 20:41:10 +0300

    Add Rivers C++

---
+++ Project/Website/C++/Rivers/Rivers/Rivers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void rivers() {
+    cout << "Iskar - 368 Kilometers Lenght" << endl;
+    cout << "Maritsa - 480 Kilometers Lenght" << endl;
+    cout << "Struma - 415 Kilometers Lenght" << endl;
+    cout << "Kamchiya - 191 Kilometers Lenght" << endl;
+    cout << "Tundzha - 328 Kilometers Lenght" << endl;
+    cout << endl;
+}
+
+void text() {
+    cout << "Press 1 to Exit" << endl;
+}
+
+int main()
+{
+    rivers();
+    int a;
+    text();
+    cin >> a;
+}
